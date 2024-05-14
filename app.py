595096528ec9d097deebcebfb2d2a6ee568ffb79
SWngugi <nyoikestan@gmail.com>
2024-05-14 13:00:18 +0300

    first Host

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP], title="Superstore Analysis")
+server = app.server
 
 sidebar = dbc.Col(
     children=[
@@ -30,4 +31,4 @@ app.layout = html.Div(
 )
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader = True, dev_tools_hot_reload = True, dev_tools_hot_reload_interval = 10, dev_tools_ui = True)
+    app.run_server(debug=False, use_reloader = True, dev_tools_hot_reload = True, dev_tools_hot_reload_interval = 10, dev_tools_ui = True)
